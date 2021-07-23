bdcf81ccc16f734bb64d60273c194e46991ca389
Vladomira <juliavladomira@gmail.com>
2021-07-23 15:06:16 +0300

    добавляет карточки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './sass/main.scss';
+
+// импорт массива блюд
+import dishes from './menu.json';
+// все карточки
+import allCards from './templates/all_cards.hbs';
+
+
+
+const dishesList = document.querySelector('.js-menu');
+// console.log(dishesList);
+
+
+const cardDish = allCards(dishes);
+// console.log(cardDish);
+dishesList.insertAdjacentHTML('beforeend', cardDish);
